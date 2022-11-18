6e5132e3537069e211d89b497c1a1a1aed64243d
RGBCube <RGBCube@users.noreply.github.com>
2022-11-18 20:38:20 +0300

    Cast BasicColor to Color

--- color/constants.v
+++ color/constants.v
@@ -3,22 +3,22 @@ module color
 import term
 
 pub const (
-	black          = BasicColor.black
-	red            = BasicColor.red
-	green          = BasicColor.green
-	yellow         = BasicColor.yellow
-	blue           = BasicColor.blue
-	magenta        = BasicColor.magenta
-	cyan           = BasicColor.cyan
-	white          = BasicColor.white
-	bright_black   = BasicColor.bright_black
-	bright_red     = BasicColor.bright_red
-	bright_green   = BasicColor.bright_green
-	bright_yellow  = BasicColor.bright_yellow
-	bright_blue    = BasicColor.bright_blue
-	bright_magenta = BasicColor.bright_magenta
-	bright_cyan    = BasicColor.bright_cyan
-	bright_white   = BasicColor.bright_white
+	black          = Color(BasicColor.black)
+	red            = Color(BasicColor.red)
+	green          = Color(BasicColor.green)
+	yellow         = Color(BasicColor.yellow)
+	blue           = Color(BasicColor.blue)
+	magenta        = Color(BasicColor.magenta)
+	cyan           = Color(BasicColor.cyan)
+	white          = Color(BasicColor.white)
+	bright_black   = Color(BasicColor.bright_black)
+	bright_red     = Color(BasicColor.bright_red)
+	bright_green   = Color(BasicColor.bright_green)
+	bright_yellow  = Color(BasicColor.bright_yellow)
+	bright_blue    = Color(BasicColor.bright_blue)
+	bright_magenta = Color(BasicColor.bright_magenta)
+	bright_cyan    = Color(BasicColor.bright_cyan)
+	bright_white   = Color(BasicColor.bright_white)
 )
 
 pub const (
