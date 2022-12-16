7ab21e05d353cb712f1af493d6f70d3b1a7f942b
RGBCube <RGBCube@users.noreply.github.com>
2022-12-16 21:46:29 +0300

    Make StyleImpl more performant and better

--- src/style.v
+++ src/style.v
@@ -1,7 +1,5 @@
 module color
 
-import term
-
 // Style is an interface for a style.
 pub interface Style {
 	render(string) string // Renders the string with the given style.
@@ -17,36 +15,14 @@ pub fn (s Style) println(str string) {
 	println(s.render(str))
 }
 
-enum StyleImpl {
-	reset
-	bold
-	dim
-	italic
-	underline
-	slow_blink
-	rapid_blink
-	inverse
-	hidden
-	strikethrough
+struct StyleImpl {
+	render_fn fn (string) string
 }
 
 fn (s StyleImpl) render(str string) string {
 	return if no_color {
 		str
 	} else {
-		func := match s {
-			.reset { term.reset }
-			.bold { term.bold }
-			.dim { term.dim }
-			.italic { term.italic }
-			.underline { term.underline }
-			.slow_blink { term.slow_blink }
-			.rapid_blink { term.rapid_blink }
-			.inverse { term.inverse }
-			.hidden { term.hidden }
-			.strikethrough { term.strikethrough }
-		}
-
-		func(str)
+		s.render_fn(str)
 	}
 }
